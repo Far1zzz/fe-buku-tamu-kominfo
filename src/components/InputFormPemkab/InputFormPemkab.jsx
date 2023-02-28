7b4020c860048939a147b16c51eb5f0307b85f9c
farizz <fairizwalker@gmail.com>
2023-03-01 00:00:17 +0700

    dev-formPemkab

---
+++ src/components/InputFormPemkab/InputFormPemkab.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "../../assets/css/InputFormUmum.css";
+import FormPemkab from "../FormPemkab/FormPemkab";
+import WebcamPemkab from "../WebcamPemkab/WebcamPemkab";
+
+const InputFormPemkab = () => {
+  const FormTitle = ["Ambil Foto", "Silahkan Isi Data Diri Anda"];
+  const [page, setPage] = useState(0);
+
+  const [formData, setFormData] = useState({
+    img: "",
+    nama: "",
+    noHp: "",
+    statusPegawai: "",
+    alamat: "",
+    bidang: "",
+    jabatan: "",
+    keperluan: "",
+    detail_Keperluan: "",
+    jumlahTamu: "",
+    tujuan: "",
+  });
+
+  const DisplayPage = () => {
+    if (page === 0) {
+      return <WebcamPemkab formData={formData} setFormData={setFormData} />;
+    } else {
+      return <FormPemkab formData={formData} setFormData={setFormData} />;
+    }
+  };
+
+  return (
+    <>
+      <div className="form-input-umum">
+        <div className="header-input-umum">
+          <i class="bi bi-list"></i>
+          {FormTitle[page]}
+        </div>
+        <div className="body mt-3">{DisplayPage()}</div>
+        <div className="footer">
+          <div className="button-prev">
+            {page === FormTitle.length + 1 ? null : (
+              <button
+                onClick={() => {
+                  setPage((page) => page - 1);
+                }}
+              >
+                Prev
+              </button>
+            )}
+          </div>
+          <div className="button-next-submit">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                if (page === FormTitle.length - 1) {
+                  console.log(formData);
+                } else {
+                  setPage((page) => page + 1);
+                }
+              }}
+            >
+              {page === FormTitle.length - 1 ? "Submit" : "Next"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InputFormPemkab;
--- src/pages/FormTamuAreaPemkab/FormTamuPemkab.jsx
+++ src/pages/FormTamuAreaPemkab/FormTamuPemkab.jsx
@@ -1,7 +1,232 @@
+import { Layout, Menu } from "antd";
 import React from "react";
+import "../../assets/css/FormInstansiLuar.css";
+import Clock from "react-live-clock";
+import InputFormInstansiLuar from "../../components/InputFormInstansiLuar/InputFormInstansiLuar";
+import BanyuasinBangkit from "../../assets/icons/BanyuasinBangkit.png";
+import Gerakan from "../../assets/icons/12gerakan.png";
+import Program from "../../assets/icons/7program.png";
+import InputFormPemkab from "../../components/InputFormPemkab/InputFormPemkab";
+
+const { Header, Content, Sider } = Layout;
 
 const FormTamuPemkab = () => {
-  return <div>FormTamuPemkab</div>;
+  const date = new Date();
+
+  const tahun = date.getFullYear(),
+    tanggal = date.getDate();
+
+  const months = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ],
+    monthName = months[date.getMonth()];
+
+  const namaHari = [
+      "Minggu",
+      "Senin",
+      "Selasa",
+      "Rabu",
+      "Kamis",
+      "Jum'at",
+      "Sabtu",
+    ],
+    dayName = namaHari[date.getDay()];
+
+  const result = dayName + ", " + tanggal + " " + monthName + " " + tahun;
+
+  return (
+    <Layout style={{ color: "white" }}>
+      <Header
+        style={{
+          color: "white",
+          boxShadow: "0px 4px 4px #ffffff",
+          background:
+            "radial-gradient(96.83% 12280.84% at 3.17% 50%, #AE1D1D 0%, #4E2222 100%)",
+        }}
+      >
+        <div className="header-buku-tamu">
+          <div className="title-header">
+            <h5>E-Buku</h5>
+          </div>
+          <div className="icon-header">
+            <img src={Gerakan} alt="" />
+            <img src={Program} alt="" />
+            <img src={BanyuasinBangkit} alt="" />
+          </div>
+        </div>
+      </Header>
+      <Content>
+        <Layout>
+          <Sider
+            width={300}
+            style={{
+              height: "100%",
+              backgroundColor: "#4E2222",
+            }}
+          >
+            <Menu
+              style={{
+                background: "#4E2222",
+                color: "white",
+                padding: "40px 0 0 0",
+              }}
+            >
+              <Menu.Item
+                style={{
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                  borderRadius: 0,
+                  width: "100%",
+                  margin: "0 0 18px 0",
+                  fontSize: "17px",
+                }}
+              >
+                <div className="menu-item">
+                  <i class="bi bi-house-fill"></i>
+                  <b>Menu</b>
+                </div>{" "}
+              </Menu.Item>
+              <Menu.Item
+                style={{
+                  width: "100%",
+                  margin: "0px 0 5px 0",
+                  borderRadius: 0,
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                }}
+              >
+                <div className="menu-item">
+                  <i class="bi bi-circle-fill"></i>
+                  <b>Visi dan Misi</b>
+                </div>{" "}
+              </Menu.Item>
+              <Menu.Item
+                style={{
+                  width: "100%",
+                  margin: "0px 0 5px 0",
+                  borderRadius: 0,
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                }}
+              >
+                <div className="menu-item">
+                  <i class="bi bi-circle-fill"></i>
+                  <b>7 Program Prioritas</b>
+                </div>
+              </Menu.Item>
+              <Menu.Item
+                style={{
+                  width: "100%",
+                  margin: "0px 0 16px 0",
+                  borderRadius: 0,
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                }}
+              >
+                <div className="menu-item">
+                  <i class="bi bi-circle-fill"></i>
+                  <b>12 Gerakan Bersama Masyarakat</b>
+                </div>
+              </Menu.Item>
+
+              <Menu.Item
+                style={{
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                  borderRadius: 0,
+                  width: "100%",
+                  margin: "0 0 16px 0",
+                }}
+              >
+                <div className="menu-item">
+                  <i class="bi bi-graph-up"></i>
+                  <b>JUMLAH PENGUNJUNG</b>
+                </div>{" "}
+              </Menu.Item>
+              <Menu.Item
+                style={{
+                  background:
+                    "radial-gradient(96.83% 12280.84% at 3.17% 50%, #EA2626 0%, rgba(255, 255, 255, 0) 100%)",
+                  borderRadius: 0,
+                  width: "100%",
+                  height: "100%",
+                  margin: "0 0 16px 0",
+                }}
+              >
+                <div className="format-tanggal">
+                  <i class="bi bi-calendar-week">Hari ini</i>
+                  <span> 2 Orang</span>
+                </div>
+                <div className="format-tanggal">
+                  <i class="bi bi-calendar3">Bulan Ini </i>
+                  <span>10 Orang</span>
+                </div>
+                <div className="garis">
+                  <hr />
+                </div>
+                <div className="format-tanggal">
+                  <i class="bi bi-bar-chart-line">Total</i>
+                  <span>12 orang</span>
+                </div>
+              </Menu.Item>
+              <Menu.Item
+                style={{
+                  cursor: "default",
+                  backgroundColor: "white",
+                  borderRadius: 0,
+                  height: "100%",
+                  width: "100%",
+                  margin: "25px 0 0 0",
+                  color: "#EA2626",
+                  fontFamily: "Poppins",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  fontSize: "15px",
+                  padding: 0,
+                  textAlign: "center",
+                }}
+              >
+                <div className="tanggal-hari-ini">
+                  <i class="bi bi-calendar3"></i>
+                  {result}
+                </div>
+                <div
+                  style={{
+                    backgroundColor: "#EA2626",
+                    color: "white",
+                    padding: "10px",
+                  }}
+                >
+                  <div className="lingkaran">
+                    <Clock format={"HH:mm:ss"} ticking={true} className="jam" />
+                  </div>
+                </div>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content
+            className="content-form"
+            style={{
+              padding: "20px",
+            }}
+          >
+            <InputFormPemkab />
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
+  );
 };
 
 export default FormTamuPemkab;
